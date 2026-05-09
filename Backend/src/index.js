1e7517014395d425a5b146f3a66bf809aec387cc
UJJWAL 7777 <unarayan7777@gmail.com>
2026-05-09 12:44:47 +0530

    fixed final cors

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -24,10 +24,19 @@ const cors = require('cors')
 // }))
 
 app.use(cors({
-    origin: ['https://code-black-six.vercel.app'],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    origin: function (origin, callback) {
+        const allowedOrigins = [
+            'http://localhost:5173',
+            'https://code-black-six.vercel.app'
+        ];
+
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true
 }));
 
 app.use(express.json());
