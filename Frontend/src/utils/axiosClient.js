054be10c2d9868e99c62e34da40a2ef17d03180a
UJJWAL 7777 <unarayan7777@gmail.com>
2026-05-09 12:58:57 +0530

    updated backend to new render

--- Frontend/src/utils/axiosClient.js
+++ Frontend/src/utils/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosClient = axios.create({
-    baseURL: 'https://code-black-tmc9.onrender.com',
+    baseURL: 'https://code-black-1.onrender.com',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
